f5a8435f88436ff59c8f04de616c3828929531ea
EkaterinaKomarova <katia.komarovaa@yandex.ru>
2017-03-10 15:36:52 +0500

    10.03.17 №2

---
+++ js/script.js
@@ -0,0 +1,15 @@
+var map;
+function init () {
+map = new YMaps.Map(document.getElementById("yandex-map"));
+
+var s = new YMaps.Style();
+s.iconStyle = new YMaps.IconStyle();
+s.iconStyle.href = "img/Map_IceCream.png";
+s.iconStyle.size = new YMaps.Point(218, 142);
+var point = new YMaps.GeoPoint(59.938631,30.323055);
+var placemark = new YMaps.Placemark(point, {style: s});
+map.addOverlay(placemark);
+}
+
+
+/*Координаты маркера 59.938631,30.323055 */
