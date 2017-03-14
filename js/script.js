01326030abbbb6db3f49ff4f398738b82852be08
EkaterinaKomarova <katia.komarovaa@yandex.ru>
2017-03-14 16:33:59 +0500

    14.03.2017

--- js/script.js
+++ js/script.js
@@ -1,3 +1,5 @@
+/* Маркер на карте */
+/* Координаты маркера 59.938631,30.323055 */
 var map;
 function init () {
 map = new YMaps.Map(document.getElementById("yandex-map"));
@@ -10,6 +12,3 @@ var point = new YMaps.GeoPoint(59.938631,30.323055);
 var placemark = new YMaps.Placemark(point, {style: s});
 map.addOverlay(placemark);
 }
-
-
-/*Координаты маркера 59.938631,30.323055 */
